b74e8cd6433d69b9fd730d6d09df587590913aee
mirror <mirror12k@gmail.com>
2016-02-18 20:07:05 -0500

    some clean up and commenting of Carbon::Anthracite::HTMLParser

--- Carbon/Anthracite/HTMLParser.pm
+++ Carbon/Anthracite/HTMLParser.pm
@@ -5,21 +5,27 @@ use warnings;
 use feature 'say';
 
 
+
+# a simple html parser which parses out tags, text, and directives
+
+
 sub new {
 	my ($class, $text) = @_;
-	my $self = bless { text => $text, position => 0 }, $class;
+	my $self = bless { text => $text, complete => 0 }, $class;
 
 	return $self
 }
 
 
+# parses text and returns the next token in text or undef if parsing is done
+# token is a list describing the token, the first value being the type of token
 sub get_token {
 	my ($self) = @_;
-	return if $self->{position} >= length $self->{text};
 
-	# my $text = $self->{text};
-	# pos($self->{text}) = $self->{position};
+	# don't restart parsing after we've already completed it
+	return if $self->{complete};
 
+	# get a token from text
 	while ($self->{text} =~
 			m/\G
 				(<([!\/]?[a-zA-Z]+\b)(.*?)>)| # match a start tag, end tag, or comment
@@ -29,8 +35,8 @@ sub get_token {
 				(.+?)(?:(?=<)|\Z) # match regular text
 			/smxg) {
 		my ($tag, $tag_type, $tag_data, $directive, $directive_type, $directive_data, $text) = ($1, $2, $3, $4, $5, $6, $7);
-		# say "debug: ", pos $self->{text};
-		# $self->{position} = pos $self->{text};
+
+		# parse the result
 		if (defined $tag) {
 			return tag => $tag, $tag_type, $tag_data
 		} elsif (defined $directive) {
@@ -39,6 +45,9 @@ sub get_token {
 			return text => $text
 		}
 	}
+	# since the regex stopped matching, we have completed the search and should not return
+	$self->{complete} = 1;
+
 	return
 }
 
